faf408006bbfdad2d6e16023d576f17e63401129
Joshua Bourgeot <dragonsploder@gmail.com>
2021-01-21 19:38:19 -0800

    Working player movement

--- Katana.c
+++ Katana.c
@@ -53,22 +53,170 @@ void main(){
 
 void gameLoop() {
     int command = 0;
-
+    int selectedKatana = -1;
 
     do {
+        switch (command) {
+            case TOP_LEFT_KATANA: {
+                selectedKatana = 0;
+                break;
+            }
+            case TOP_RIGHT_KATANA: {
+                selectedKatana = 1;
+                break;
+            }
+            case BOTTOM_LEFT_KATANA: {
+                selectedKatana = 2;
+                break;
+            }
+            case BOTTOM_RIGHT_KATANA: {
+                selectedKatana = 3;
+                break;
+            }
+            
+            default: {
+                selectedKatana = -1;
+            }
+        }
+
+        if (selectedKatana != -1) {
+            playerMove(player.katanas[selectedKatana]);
+        }
+
+
         printBoarder();
         printMap();
         for (int i = 0; i < 4; i++) {
             printKatana(player.katanas[i], i);
         }
-        //printEntities();
-
-
-
-
+        printEntities();
 
         command = myGetch();
     } while (command != 'q');
+}
+
+/* Movement */
+
+/*
+#define MOVEMENT_STRIKE   1 // Move to strongest enemy                        
+#define MOVEMENT_BERSERK  2 // Move to the closest enemy                      
+#define MOVEMENT_RETREAT  3 // Move away from most enemies                    
+#define MOVEMENT_RETURN   4 // Attempt to move to terrain maching katana type 
+#define MOVEMENT_DEFEND   5 // Don't move                                     
+#define MOVEMENT_RAND     6 // Move in random newLocation                       
+*/
+
+void playerMove(struct Katana katana) {
+    struct Vec2 newLocation;
+
+    switch (katana.movementType) {
+        case MOVEMENT_STRIKE: {
+            int strongestEnemy = 0;
+            for (int i = 0; i < currentNumberOfEnemies; i++) {
+                if (enemies[i].level > enemies[strongestEnemy].level) {
+                    strongestEnemy = i;
+                }
+            }
+            newLocation = pathFinding(player.location, enemies[strongestEnemy].location);
+            break;
+        }
+
+        case MOVEMENT_BERSERK: {
+            int closestEnemy = 0;
+            int closestEnemyDistance = MAP_HEIGHT + MAP_WIDTH;
+            for (int i = 0; i < currentNumberOfEnemies; i++) {
+                int currentEnemyDistance = findDistance(player.location, enemies[i].location);
+                if (currentEnemyDistance < closestEnemyDistance) {
+                    closestEnemy = i;
+                    closestEnemyDistance = currentEnemyDistance;
+                }
+            }
+            newLocation = pathFinding(player.location, enemies[closestEnemy].location);
+            break;
+        }
+        
+        case MOVEMENT_RETREAT: { /* I can't think of a better way to do this */
+            double distances[9];
+            distances[0] = findDistanceToClosestEnemy((struct Vec2) {player.location.y + 1, player.location.x + 1});
+            distances[1] = findDistanceToClosestEnemy((struct Vec2) {player.location.y + 1, player.location.x + 0});
+            distances[2] = findDistanceToClosestEnemy((struct Vec2) {player.location.y + 1, player.location.x - 1});
+            distances[3] = findDistanceToClosestEnemy((struct Vec2) {player.location.y + 0, player.location.x + 1});
+            distances[4] = findDistanceToClosestEnemy((struct Vec2) {player.location.y + 0, player.location.x + 0});
+            distances[5] = findDistanceToClosestEnemy((struct Vec2) {player.location.y + 0, player.location.x - 1});
+            distances[6] = findDistanceToClosestEnemy((struct Vec2) {player.location.y - 1, player.location.x + 1});
+            distances[7] = findDistanceToClosestEnemy((struct Vec2) {player.location.y - 1, player.location.x + 0});
+            distances[8] = findDistanceToClosestEnemy((struct Vec2) {player.location.y - 1, player.location.x - 1});
+
+            int farthestDistanceIndex = 0;
+            do {
+                newLocation = player.location;
+                
+                for (int i = 0; i < 9; i++) {
+                    if (distances[i] > distances[farthestDistanceIndex]) {
+                        farthestDistanceIndex = i;
+                    }
+                }
+
+                switch (farthestDistanceIndex) {
+                    case 0: { newLocation.y += 1; newLocation.x += 1; break; }
+                    case 1: { newLocation.y += 1; newLocation.x += 0; break; }
+                    case 2: { newLocation.y += 1; newLocation.x -= 1; break; }
+                    case 3: { newLocation.y += 0; newLocation.x += 1; break; }
+                    case 4: { newLocation.y += 0; newLocation.x += 0; break; }
+                    case 5: { newLocation.y += 0; newLocation.x -= 1; break; }
+                    case 6: { newLocation.y -= 1; newLocation.x += 1; break; }
+                    case 7: { newLocation.y -= 1; newLocation.x += 0; break; }
+                    case 8: { newLocation.y -= 1; newLocation.x -= 1; break; }
+
+                    default:{ ERROR("This shouldn't be possible");           }
+                }
+
+                distances[farthestDistanceIndex] = 0; /* If this option is out of bounds, make sure we don't pick it again */
+            } while ((newLocation.y < 0 || newLocation.y >= MAP_HEIGHT) || (newLocation.x < 0 || newLocation.x >= MAP_WIDTH));
+            
+            
+            break;
+        }
+        
+        case MOVEMENT_RETURN: { /* This could be more efficient, I know */
+            struct Vec2 closestTerrain = (struct Vec2) {0, 0};
+            int closestTerrainDistance = MAP_HEIGHT + MAP_WIDTH;
+            for (int y = 0; y < MAP_HEIGHT; y++) {
+                for (int x = 0; x < MAP_WIDTH; x++) {
+                    if (map[y][x].type == katanaToTerrain[katana.katanaType]) {
+                        int currentTerrainDistance = findDistance(player.location, (struct Vec2) {y, x});
+                        if (currentTerrainDistance < closestTerrainDistance) {
+                            closestTerrain = (struct Vec2) {y, x};
+                            closestTerrainDistance = currentTerrainDistance;
+                        }
+                    }
+                }
+            }
+            newLocation = pathFinding(player.location, closestTerrain);
+            break;
+        }
+        
+        case MOVEMENT_DEFEND: {
+            newLocation = player.location;
+            break;
+        }
+        
+        case MOVEMENT_RAND: {
+            do {
+                newLocation = player.location;
+                newLocation.y += (rand() % 3) - 1;
+                newLocation.x += (rand() % 3) - 1;
+            } while ((newLocation.y == player.location.y && newLocation.x == player.location.x) || ((newLocation.y < 0 || newLocation.y >= MAP_HEIGHT) || (newLocation.x < 0 || newLocation.x >= MAP_WIDTH)));
+            break;
+        }
+
+        default: {
+            ERROR("Invalid movementType argument");
+            break;
+        }
+    }
+
+    player.location = newLocation;
 }
 
 
@@ -76,31 +224,43 @@ void gameLoop() {
 
 void genEnemy() {
     if (currentNumberOfEnemies < MAX_NUMBER_OF_ENEMIES) {
+        enemies[currentNumberOfEnemies].type = rand() % NUMBER_OF_ENEMY_TYPES;
+
+        enemies[currentNumberOfEnemies].health = dice(2, 5);
+        enemies[currentNumberOfEnemies].power = dice(2, 5);
+        enemies[currentNumberOfEnemies].level = enemies[currentNumberOfEnemies].health * enemies[currentNumberOfEnemies].power;
+
+
         struct Vec2 location = (struct Vec2) {0, 0};
         do {
             switch (rand() % 4) {
-                case 0: /* Right */
+                case 0: {/* Right */
                     location.y = rand() % MAP_HEIGHT;
                     location.x = 0;
                     break;
-                case 1: /* Left */
+                }
+                case 1: {/* Left */
                     location.y = rand() % MAP_HEIGHT;
                     location.x = MAP_WIDTH - 1;
                     break;
-                case 2: /* Top */
+                }
+                case 2: {/* Top */
                     location.y = 0;
                     location.x = rand() % MAP_WIDTH;
                     break;
-                case 3: /* Bottom */
+                }
+                case 3: {/* Bottom */
                     location.y = MAP_HEIGHT - 1;
                     location.x = rand() % MAP_WIDTH;
                     break;
-                default:
+                }
+                default: {
                     ERROR("Rand has failed me");
+                }
             }
         } while (checkForEnemy(location) != 0);
 
-        enemies[currentNumberOfEnemies] = (struct Enemy) {rand() % NUMBER_OF_ENEMY_TYPES, 10, 10, location};
+        enemies[currentNumberOfEnemies].location = location;
         currentNumberOfEnemies++;
     }
 }
@@ -124,8 +284,8 @@ void genMap() {
             map[y][x].type = TERRAIN_DIRT;
         }
     }
-    for (int i = 0; i < 15; i++) {
-        terrainAreaMap((rand() % (NUMBER_OF_TERRAIN_TYPES - 1)) + 1, (struct Vec2) {rand() % MAP_HEIGHT, rand() % MAP_WIDTH}, (rand() % 5) + 2);
+    for (int i = 0; i < 20; i++) {
+        terrainAreaMap(rand() % NUMBER_OF_TERRAIN_TYPES, (struct Vec2) {rand() % MAP_HEIGHT, rand() % MAP_WIDTH}, (rand() % 5) + 2);
     }
 }
 
@@ -169,6 +329,45 @@ void printError(char *message, char *file, int line){
     printf("   Error: %s\n", message);
     exit(-1);
 }
+
+double findDistance(struct Vec2 start, struct Vec2 end) {
+    return fmax(abs(end.y - start.y), abs(end.x - start.x));
+}
+
+double findDistanceToClosestEnemy(struct Vec2 location) {
+    int closestEnemy = 0;
+    int closestEnemyDistance = MAP_HEIGHT + MAP_WIDTH;
+    for (int i = 0; i < currentNumberOfEnemies; i++) {
+        int currentEnemyDistance = findDistance(location, enemies[i].location);
+        if (currentEnemyDistance < closestEnemyDistance) {
+            closestEnemy = i;
+            closestEnemyDistance = currentEnemyDistance;
+        }
+    }
+    return closestEnemyDistance;
+}
+
+struct Vec2 pathFinding(struct Vec2 start, struct Vec2 end) {
+    struct Vec2 step = start;
+
+    if (end.y > start.y) {
+        step.y += 1;
+    } else if (end.y < start.y) {
+        step.y -= 1;
+    }
+
+    if (end.x > start.x) {
+        step.x += 1;
+    } else if (end.x < start.x) {
+        step.x -= 1;
+    }
+
+    if ((step.y < 0 || step.y > MAP_HEIGHT) || (step.x < 0 || step.x > MAP_WIDTH)) {
+        return start;
+    }
+
+    return step;
+}   
 
 
 /* Curses IO Functions */
@@ -272,62 +471,67 @@ void printKatana(struct Katana katana, int position) {
     struct Vec2 bottomRightCorner = {0, 0};
     struct Vec2 bottomLeftCorner = {0, 0};
     
-    switch(position) { 
-        case 0:
-            topRightCorner.y = (SCREEN_HEIGHT / 3) + 1;
-            topRightCorner.x = 1;
+    switch(position) {
+        case 0: {/* Top left katana */
+            topLeftCorner.y = (SCREEN_HEIGHT / 3) + 1;
+            topLeftCorner.x = 1;
             break;
-        case 1:
-            topRightCorner.y = (SCREEN_HEIGHT / 3) + 1;
-            topRightCorner.x = (SCREEN_WIDTH - 2) - katanaInfoBoxWidth;
+        }
+        case 1: {/* Top right katana */
+            topLeftCorner.y = (SCREEN_HEIGHT / 3) + 1;
+            topLeftCorner.x = (SCREEN_WIDTH - 2) - katanaInfoBoxWidth;
             break;
-        case 2:
-            topRightCorner.y = (SCREEN_HEIGHT / 3) + 2 + katanaInfoBoxHeight;
-            topRightCorner.x = 1;
+        }
+        case 2: {/* Bottom left katana */
+            topLeftCorner.y = (SCREEN_HEIGHT / 3) + 2 + katanaInfoBoxHeight;
+            topLeftCorner.x = 1;
             break;
-        case 3:
-            topRightCorner.y = (SCREEN_HEIGHT / 3) + 2 + katanaInfoBoxHeight;
-            topRightCorner.x = (SCREEN_WIDTH - 2) - katanaInfoBoxWidth;
+        }
+        case 3: {/* Bottom right katana */
+            topLeftCorner.y = (SCREEN_HEIGHT / 3) + 2 + katanaInfoBoxHeight;
+            topLeftCorner.x = (SCREEN_WIDTH - 2) - katanaInfoBoxWidth;
             break;
-        default:
+        }
+        default: {
             ERROR("Invalid position argument");
+        }
     }
 
-    topLeftCorner.y = topRightCorner.y;
-    topLeftCorner.x = topRightCorner.x + katanaInfoBoxWidth;
+    topRightCorner.y = topLeftCorner.y;
+    topRightCorner.x = topLeftCorner.x + katanaInfoBoxWidth;
 
-    bottomRightCorner.y = topRightCorner.y + katanaInfoBoxHeight;
-    bottomRightCorner.x = topRightCorner.x;
+    bottomRightCorner.y = topLeftCorner.y + katanaInfoBoxHeight;
+    bottomRightCorner.x = topLeftCorner.x;
 
-    bottomLeftCorner.y = topRightCorner.y + katanaInfoBoxHeight;
-    bottomLeftCorner.x = topRightCorner.x + katanaInfoBoxWidth;
-
-    
-    printVerticalLine(topRightCorner.x, topRightCorner.y, bottomRightCorner.y, "|");
-    printVerticalLine(topLeftCorner.x, topRightCorner.y, bottomRightCorner.y, "|");
-
-    printHorizontalLine(topRightCorner.y, topRightCorner.x, topLeftCorner.x, "-");
-    printHorizontalLine(bottomLeftCorner.y, topRightCorner.x, topLeftCorner.x, "-");
+    bottomLeftCorner.y = topLeftCorner.y + katanaInfoBoxHeight;
+    bottomLeftCorner.x = topLeftCorner.x + katanaInfoBoxWidth;
 
     
-    mvprintw(topRightCorner.y, topRightCorner.x, &katanaCornerIcon[katana.katanaType][0]);
+    printVerticalLine(topLeftCorner.x, topLeftCorner.y, bottomRightCorner.y, "|");
+    printVerticalLine(topRightCorner.x, topLeftCorner.y, bottomRightCorner.y, "|");
+
+    printHorizontalLine(topLeftCorner.y, topLeftCorner.x, topRightCorner.x, "-");
+    printHorizontalLine(bottomLeftCorner.y, topLeftCorner.x, topRightCorner.x, "-");
+
+    
     mvprintw(topLeftCorner.y, topLeftCorner.x, &katanaCornerIcon[katana.katanaType][0]);
+    mvprintw(topRightCorner.y, topRightCorner.x, &katanaCornerIcon[katana.katanaType][0]);
     mvprintw(bottomRightCorner.y, bottomRightCorner.x, &katanaCornerIcon[katana.katanaType][0]);
     mvprintw(bottomLeftCorner.y, bottomLeftCorner.x, &katanaCornerIcon[katana.katanaType][0]);
 
     char buffer[katanaInfoBoxWidth - 2];
 
     
-    mvprintw(topRightCorner.y + 1, (topRightCorner.x + (katanaInfoBoxWidth/2)) - (strlen(katana.name)/2), katana.name);
-
+    mvprintw(topLeftCorner.y + 1, (topLeftCorner.x + (katanaInfoBoxWidth/2)) - (strlen(katana.name)/2), katana.name);
+    
     sprintf(buffer,"Dammage: %i", katana.damageAmount);
-    mvprintw(topRightCorner.y + 3, (topRightCorner.x + (katanaInfoBoxWidth/2)) - (strlen(buffer)/2), buffer);
+    mvprintw(topLeftCorner.y + 3, (topLeftCorner.x + (katanaInfoBoxWidth/2)) - (strlen(buffer)/2), buffer);
 
     sprintf(buffer, "Hit Chance: %i", katana.hitChance);
-    mvprintw(topRightCorner.y + 4, (topRightCorner.x + (katanaInfoBoxWidth/2)) - (strlen(buffer)/2), buffer);
+    mvprintw(topLeftCorner.y + 4, (topLeftCorner.x + (katanaInfoBoxWidth/2)) - (strlen(buffer)/2), buffer);
 
     sprintf(buffer, "Movement Type: %s", katanaMovementTypeNames[katana.movementType]);
-    mvprintw(topRightCorner.y + 5, (topRightCorner.x + (katanaInfoBoxWidth/2)) - (strlen(buffer)/2), buffer);
+    mvprintw(topLeftCorner.y + 5, (topLeftCorner.x + (katanaInfoBoxWidth/2)) - (strlen(buffer)/2), buffer);
     
     
 }
--- Katana.h
+++ Katana.h
@@ -15,6 +15,12 @@
 
 #define ERROR(x) printError(#x,__FILE__, __LINE__)
 
+/* Controls */
+#define TOP_LEFT_KATANA 'u'
+#define TOP_RIGHT_KATANA 'i'
+#define BOTTOM_LEFT_KATANA 'j'
+#define BOTTOM_RIGHT_KATANA 'k'
+
 /* Screen laylout def */
 #define SCREEN_HEIGHT 24
 #define SCREEN_WIDTH 80
@@ -43,34 +49,36 @@
 
 #define NUMBER_OF_KATANA_TYPES 6
 
-#define KATANA_FIRE      1
-#define KATANA_ICE       2
-#define KATANA_WIND      3
-#define KATANA_STONE     4
-#define KATANA_LIGHTNING 5
-#define KATANA_POSION    6
+#define KATANA_FIRE      0
+#define KATANA_ICE       1
+#define KATANA_WIND      2
+#define KATANA_STONE     3
+#define KATANA_LIGHTNING 4
+#define KATANA_POSION    5
 
-#define NUMBER_OF_MOVEMENT_TYPES 4
+#define NUMBER_OF_MOVEMENT_TYPES 6
 
-#define MOVEMENT_ATTACK  1
-#define MOVEMENT_RETREAT 2
-#define MOVEMENT_WAIT    3
-#define MOVEMENT_RAND    4
+#define MOVEMENT_STRIKE   0 /* Move to strongest enemy                        */
+#define MOVEMENT_BERSERK  1 /* Move to the closest enemy                      */
+#define MOVEMENT_RETREAT  2 /* Move away from most enemies                    */
+#define MOVEMENT_RETURN   3 /* Attempt to move to terrain maching katana type */
+#define MOVEMENT_DEFEND   4 /* Don't move                                     */
+#define MOVEMENT_RAND     5 /* Move in random direction                       */
 
 
 #define NUMBER_OF_TERRAIN_TYPES 4
 
-#define TERRAIN_DIRT  0
-#define TERRAIN_WATER 1
-#define TERRAIN_ROCK  2
-#define TERRAIN_MAGMA 3
+#define TERRAIN_DIRT  0 /* None */
+#define TERRAIN_WATER 1 /* Ice & Lightning synergies */
+#define TERRAIN_ROCK  2 /* Stone & Wind synergies    */
+#define TERRAIN_MAGMA 3 /* Fire & Posion synergies   */
 
 #define NUMBER_OF_ENEMY_TYPES 4
 
-#define ENEMY_BASIC  0
+#define ENEMY_BASIC   0
 #define ENEMY_ENERGY  1 /* Fire & Lightning resistant */
-#define ENEMY_STRONG  2 /* Stone & Wind resistant */
-#define ENEMY_FLUID   3 /* Ice and Posion resistant */
+#define ENEMY_STRONG  2 /* Stone & Wind resistant     */
+#define ENEMY_FLUID   3 /* Ice & Posion resistant     */
 
 
 
@@ -108,6 +116,7 @@ struct Enemy {
     int type;
     int health;
     int power;
+    int level; /* health * power */
 
     struct Vec2 location;
 
@@ -152,10 +161,21 @@ const char katanaCornerIcon[NUMBER_OF_KATANA_TYPES][2] = {
     "~"  /* Posion    */
 };
 
+const int katanaToTerrain[NUMBER_OF_KATANA_TYPES] = {
+    3, /* Fire -> Magma      */
+    1, /* Ice -> Water       */
+    2, /* Wind -> Rock       */
+    2, /* Stone -> Rock      */
+    3, /* Lightning -> Magma */
+    1, /* Posion -> Water    */
+};
+
 const char katanaMovementTypeNames[NUMBER_OF_MOVEMENT_TYPES][10] = {
-    "Attack",
+    "Strike",
+    "Berserk",
+    "Retreat",
+    "Return",
     "Defend",
-    "Wait",
     "Random"
 };
 
@@ -169,9 +189,9 @@ const char katanaNameType[NUMBER_OF_KATANA_TYPES][10] = {
 };
 
 const char katanaNameDamage[KATANA_MAX_DAMAGE][10] = {
-    "usagi",   /* Rabbit  */
     "kaeru",   /* Frog    */
     "sakana",  /* Fish    */
+    "usagi",   /* Rabbit  */
     "tori",    /* Bird    */
     "neko",    /* Cat     */
     "inu",     /* Dog     */
@@ -186,6 +206,8 @@ const char katanaNameDamage[KATANA_MAX_DAMAGE][10] = {
 /*==== Forward Function Decleration =======================================================================*/
 void main();                                                         /* Main function                      */
 void gameLoop();                                                     /* Game loop which runs every turn    */
+/*---- Movement Functions ---------------------------------------------------------------------------------*/
+void playerMove(struct Katana katana);                               /* Moves the player                   */
 /*---- Generator Functions --------------------------------------------------------------------------------*/
 void genEnemy();                                                     /* Generate a random enemy            */
 void genKatana(struct Katana *katana);                               /* Generate a random Katana           */
@@ -196,6 +218,9 @@ bool checkForEnemy(struct Vec2 location);                            /* Check if
 /*---- Utility Functions ----------------------------------------------------------------------------------*/
 int dice(int number, int sides);                                     /* DnD style dice rolls               */
 void printError(char* message, char* file, int line);                /* Error function used by ERROR()     */
+double findDistance(struct Vec2 start, struct Vec2 end);             /* Distance between two points        */
+double findDistanceToClosestEnemy(struct Vec2 location);             /* Function used in retreat movement  */
+struct Vec2 pathFinding(struct Vec2 start, struct Vec2 end);         /* Find next step from start to end   */
 /*---- Miscellaneous Functions ----------------------------------------------------------------------------*/
 /*---- Curses IO Functions --------------------------------------------------------------------------------*/
 void initCurses();                                                   /* Initalise Curses library           */
